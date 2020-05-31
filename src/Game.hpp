2ebde2eefdcd302cd49773e6500249270aaef3f9
iloveooz <iloveooz@yandex.ru>
2020-05-31 15:19:31 +0700

    added game states to the game

--- src/Game.hpp
+++ src/Game.hpp
@@ -1,20 +1,24 @@
 #ifndef SQUAREMAN_GAME_HPP
 #define SQUAREMAN_GAME_HPP
 
+#include <array>
+
 #include <SFML/Graphics.hpp>
+#include "GameState.hpp"
 
 class Game {
 public:
 	Game();
+	~Game();
 	
 	void run();
-	void insertCoin();
-	void pressButton();
-
-	void moveStick(sf::Vector2i direction);
+	
+	void changeGameState(GameState::State gameState);
 
 private:
 	sf::RenderWindow m_window;
+	GameState* m_currentState;
+	std::array<GameState *, GameState::Count> m_gameStates;
 };
 
 #endif // SQUAREMAN_GAME_HPP
---
+++ src/GameState.hpp
@@ -0,0 +1,94 @@
+#ifndef SQUAREMAN_GAMESTATE_HPP
+#define SQUAREMAN_GAMESTATE_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Game;
+
+class GameState {
+public:
+	enum State {
+		NoCoin, 
+		GetReady,
+		Playing,
+		Lost, 
+		Won,
+		Count
+	};
+	
+	GameState(Game* game);
+	Game* getGame() const;
+	
+	// three fundamental steps
+	virtual void insertCoin() = 0;
+	virtual void pressButton() = 0;
+	virtual void moveStick(sf::Vector2i direction) = 0;
+	
+	virtual void update(sf::Time delta) = 0; // update game states
+	virtual void draw(sf::RenderWindow& window) = 0; // drawing new content
+	
+private:
+	Game* m_game;
+};
+
+class NoCoinState : public GameState {
+public:
+	NoCoinState(Game* game);
+
+	void insertCoin();
+	void pressButton();
+	void moveStick(sf::Vector2i direction);
+	
+	void update(sf::Time delta);
+	void draw(sf::RenderWindow& window);
+};
+
+class GetReadyState : public GameState {
+public:
+	GetReadyState(Game* game);
+
+	void insertCoin();
+	void pressButton();
+	void moveStick(sf::Vector2i direction);
+	
+	void update(sf::Time delta);
+	void draw(sf::RenderWindow& window);
+};
+
+class PlayingState : public GameState {
+public:
+	PlayingState(Game* game);
+
+	void insertCoin();
+	void pressButton();
+	void moveStick(sf::Vector2i direction);
+	
+	void update(sf::Time delta);
+	void draw(sf::RenderWindow& window);
+};
+
+class LostState : public GameState {
+public:
+	LostState(Game* game);
+
+	void insertCoin();
+	void pressButton();
+	void moveStick(sf::Vector2i direction);
+	
+	void update(sf::Time delta);
+	void draw(sf::RenderWindow& window);
+};
+
+class WonState : public GameState {
+public:
+	WonState(Game* game);
+
+	void insertCoin();
+	void pressButton();
+	void moveStick(sf::Vector2i direction);
+	
+	void update(sf::Time delta);
+	void draw(sf::RenderWindow& window);
+};
+
+#endif // SQUAREMAN_GAMESTATE_HPP
