509b34ad4fc47c7d12e0b60ee1c923beb6437dc5
iloveooz <iloveooz@yandex.ru>
2020-06-03 00:28:46 +0700

    added bonuses

--- src/Bonus.hpp
+++ src/Bonus.hpp
@@ -1,6 +1,23 @@
 #ifndef SQUAREMAN_BONUS_HPP
 #define SQUAREMAN_BONUS_HPP
 
+#include <SFML/Graphics.hpp>
 
+class Bonus : public sf::Drawable, public sf::Transformable {
+public:
+	enum Fruit {
+		Banana,
+		Apple,
+		Cherry
+	};
+	
+	Bonus(sf::Texture& texture);
+	void setFruit(Fruit fruit);
+	
+private:
+	sf::Sprite m_visual;
+
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+};
 
 #endif // SQUAREMAN_BONUS_HPP
